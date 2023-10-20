acde7d75db6ab9d6720cd3586b40a090c629df40
Aryan <aryan.saxena094@gmail.com>
2023-10-20 14:22:35 -0400

    commit\

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
-  title = COURSES[0].description;
+  @ViewChildren(CourseCardComponent)
+  cards : QueryList<CourseCardComponent>;
 
-  price = 912.129841294;
-
-  startDate = new Date(2000, 0, 1);
-
-
-
-  onCourseSelected(course:Course){
-    console.log("App Component - click event bubbled to container component -", course);
+  ngAfterViewInit(): void {
+    console.log(this.courses);
   }
 
+  onCourseSelected(course: Course){
+  };
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
@@ -6,21 +6,23 @@ import { Course } from '../model/course';
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit, AfterViewInit{
   constructor() {}
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    console.log("image: ", this.image)
+  }
   
   @Input({
-    required: true
   })
   course: Course;
-
-  @Input({
-    required: true
-  })
-  cardIndex: number;
-  
+ 
   @Output('courseSelected')
   courseEmitter = new EventEmitter<Course>();
+
+  @ContentChild('courseImage')
+  image;
 
   onCourseViewed() {
     console.log("Card component - button clicked...")
